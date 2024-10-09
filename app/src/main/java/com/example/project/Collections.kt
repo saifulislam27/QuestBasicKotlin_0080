feb403a3792ae27adca30aedffe2f9332b4399e7
saifulislam27 <mariyatilambeja@gmail.com>
2024-10-10 04:51:56 +0700

    Menambahkan kelas dasar dan memodifikasi properti pada Kotlin:
    - Membuat kelas `Motor` sebagai kelas kosong untuk pengembangan di masa depan.
    - Menambahkan kelas `Contact` dengan properti `id` yang tidak bisa diubah (immutable) dan `email` yang bisa diubah (mutable).
    - Menginisialisasi instance dari kelas `Contact` dengan nilai email awal, kemudian memperbarui dan mencetak properti email.

---
+++ app/src/main/java/com/example/project/Collections.kt
@@ -0,0 +1,14 @@
+package com.example.project
+
+
+fun ContohList(){
+    println("=== List ===")
+
+    val readOnlyAbjad = listOf("A", "B", "C")
+    println(readOnlyAbjad)
+
+    val shape: MutableList<String> = mutableListOf("Circle", "Square", "Triangle")
+    println(shape)
+
+
+}
