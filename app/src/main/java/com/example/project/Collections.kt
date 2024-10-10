9a438e0b91520a5f4787271490ee776fa347630a
saifulislam27 <mariyatilambeja@gmail.com>
2024-10-10 17:23:51 +0700

    Menambahkan contoh penggunaan koleksi (List, Set, Map) pada Kotlin.
    - Membuat fungsi `ContohList()` untuk mendemonstrasikan operasi dasar pada `MutableList` seperti menambah, menghapus, dan memodifikasi elemen.
    - Membuat fungsi `ContohSet()` untuk mendemonstrasikan operasi pada `MutableSet` termasuk menambah dan menghapus elemen.
    - Membuat fungsi `ContohMap()` untuk mendemonstrasikan operasi pada `MutableMap`, seperti menambah, menghapus, dan memodifikasi nilai berdasarkan key.
    - Menambahkan contoh variabel `readOnly` yang di-lock untuk `List`, `Set`, dan `Map` setelah beberapa operasi modifikasi dilakukan.
    - Memanggil semua fungsi di dalam fungsi `main()` untuk menampilkan hasil operasinya.

--- app/src/main/java/com/example/project/Collections.kt
+++ app/src/main/java/com/example/project/Collections.kt
@@ -10,5 +10,79 @@ fun ContohList(){
     val shape: MutableList<String> = mutableListOf("Circle", "Square", "Triangle")
     println(shape)
 
+    shape.add("Circle")
+    println(shape)
+
+    shape.remove("Triangle")
+    println(shape)
+
+    shape[0] = "Oval"
+    println(shape
+    )
+
+    val shapesLocked: List<String> = shape
+    println(shapesLocked)
 
 }
+
+fun ContohSet() {
+    println()
+    println("=== Set ===")
+
+    val readOnlyAbjad = setOf("A", "B", "C")
+    println(readOnlyAbjad)
+
+    val shape: MutableSet<String> = mutableSetOf("Circle", "Square", "Triangle")
+    println(shape)
+
+    shape.add("Rectangle")
+    println(shape)
+
+    shape.remove("Circle")
+    println(shape)
+
+    val shapesLocked: Set<String> = shape
+    println(shapesLocked)
+}
+fun ContohMap() {
+
+    println()
+    println("=== Map ===")
+
+    val readOnlyShape: Map<String, Int> = mapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(readOnlyShape)
+
+    val shape: MutableMap<String, Int> = mutableMapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(shape)
+
+    shape["Reactangle"] = 4
+    println(shape)
+
+    shape.remove("Circle")
+    println(shape)
+
+    shape["Square"] = 5
+    println(shape)
+
+    val shapesLocked: Map<String, Int> = shape
+    println(shapesLocked)
+}
+
+fun main () {
+    ContohList()
+    ContohSet()
+    ContohMap()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
