69bddd28da3c2cbe442df5c47be9db0f1fdbfc59
saifulislam27 <mariyatilambeja@gmail.com>
2024-10-10 17:54:34 +0700

    Menambahkan contoh penggunaan conditional statement di Kotlin.
    - Membuat fungsi `ConditionalStatement()` untuk mendemonstrasikan penggunaan `if-else` dan `when` expression.
    - Pada `if-else`, kondisi mengecek apakah sebuah angka positif atau negatif.
    - Pada `when`, kondisi memetakan nilai integer ke nama hari berdasarkan nilai variabel `day`, dengan menangani kondisi hari tidak valid menggunakan `else`.
    - Menyederhanakan logika untuk menjelaskan dasar penggunaan conditional expression dalam Kotlin.

---
+++ app/src/main/java/com/example/project/ConditionalExpression.kt
@@ -0,0 +1,25 @@
+package com.example.project
+
+fun ConditionalStatement() {
+    println("=== Conditional Expression ===")
+
+    val number = 10
+    if (number > 0) {
+        println("Positive Number")
+    } else {
+        println("Negative Number")
+    }
+
+    val day = 1
+    when (day) {
+        1 -> println("Sunday")
+        2 -> println("Monday")
+        3 -> println("Tuesday")
+        4 -> println("Wednesday")
+        5 -> println("Thursday")
+        6 -> println("Friday")
+        7 -> println("Saturday")
+        else -> println("Invalid Day")
+    }
+
+}
