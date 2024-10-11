6f11647bdc418111f06f12130cec4135809f9ae1
saifulislam27 <mariyatilambeja@gmail.com>
2024-10-11 23:06:40 +0700

    Menambahkan dua fungsi baru: ConditionalStatement() dan Perulangan() pada project Kotlin.
    1. **ConditionalStatement()**:
       - Implementasi percabangan menggunakan `if-else` untuk mengecek apakah sebuah angka positif atau negatif.
       - Menggunakan `when` untuk menentukan hari berdasarkan angka.
       - Penanganan exception dengan `try-catch` untuk memvalidasi input string menjadi integer.
    
    2. **Perulangan()**:
       - Menambahkan contoh perulangan menggunakan `for`, `while`, dan `do-while`.
       - Menampilkan perulangan angka 1 sampai 5.
       - Menambahkan iterasi pada list `cars` untuk menampilkan tipe mobil.
    
    File `main()` digunakan untuk menjalankan kedua fungsi tersebut.

--- app/src/main/java/com/example/project/ConditionalExpression.kt
+++ app/src/main/java/com/example/project/ConditionalExpression.kt
@@ -32,3 +32,33 @@ fun ConditionalStatement() {
 
 }
 
+fun Perulangan (){
+    println()
+    println("=== Perulangan ===")
+
+for (i in 1..5){
+    println("Perulangan ke-$i")
+}
+
+    val cars = listOf("BBM", "Toyota", "Honda", "Audi")
+    for (car in cars){
+        println("Car type: $car")
+    }
+
+    var x = 1
+    while (x <= 5) {
+        println("Perulangan ke-$x")
+        x++
+    }
+
+    var y = 1
+    do {
+        println("Perulangan ke-$y")
+        y++
+    } while (y <= 5)
+}
+
+fun main() {
+    ConditionalStatement()
+    Perulangan()
+}
