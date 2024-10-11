b759a20f27ec83b556a9852a9ee65436b6467e29
saifulislam27 <mariyatilambeja@gmail.com>
2024-10-12 00:07:24 +0700

    Memperbarui fungsi `main()` dengan penggunaan fungsi `uppercaseString()` dan lambda expression:
    - Memanggil fungsi `uppercaseString("Hello")` untuk mengonversi string menjadi huruf besar.
    - Menambahkan lambda expression `{ string: String -> string.uppercase() }` untuk mengonversi string "hello" menjadi huruf besar secara langsung di dalam `println()`.
    
    Penambahan ini menunjukkan dua pendekatan berbeda untuk melakukan operasi yang sama.

--- app/src/main/java/com/example/project/LambdaFunction.kt
+++ app/src/main/java/com/example/project/LambdaFunction.kt
@@ -4,3 +4,8 @@ package com.example.project
 fun uppercaseString(string: String): String {
     return string.uppercase()
 }
+
+fun main(){
+    uppercaseString("Hello")
+    println({ string: String -> string.uppercase()}("hello"))
+}
