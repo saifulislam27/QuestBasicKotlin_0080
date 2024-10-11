61d7563291c82b54bb4c619850dfd00044b10084
saifulislam27 <mariyatilambeja@gmail.com>
2024-10-12 00:28:11 +0700

    Menambahkan operasi penghitungan panjang string:
    - Menambahkan perintah `println(neverNull.length)` untuk mencetak panjang string `neverNull`, yang tidak bisa `null`.
    - Menambahkan perintah `println(nullable?.length)` untuk mencetak panjang string `nullable` dengan null safety menggunakan safe call (`?.`). Jika `nullable` bernilai `null`, maka `null` akan dicetak tanpa error.
    - Menambahkan fungsi `strLength(notNull: String)` yang mengembalikan panjang string yang diterima sebagai argumen, menunjukkan penggunaan properti `.length` pada variabel bertipe `String`.

--- app/src/main/java/com/example/project/NullSafety.kt
+++ app/src/main/java/com/example/project/NullSafety.kt
@@ -1,14 +1,27 @@
 package com.example.project
 
+import org.jetbrains.annotations.NotNull
+
 fun nullSafety(){
     var neverNull: String = "This can't be null"
 
+
     var nullable: String? = "You can keep a null here"
+
     nullable = null
+
 
     if (neverNull == null) {
         println("inferredNonNull is null")
     } else {
         println("inerredNodNull is not null")
+    }
+
+    println(neverNull.length)
+    println(nullable?.length)
+
+
+    fun strLength(notNull: String): Int {
+        return notNull.length
     }
 }
